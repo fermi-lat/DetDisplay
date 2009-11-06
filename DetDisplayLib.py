fff1c16c20ea48c5ee4271e74302bcd9a85800cb
jrb <>
2009-11-06 23:31:01 +0000

    changes for SCons

--- DetDisplayLib.py
+++ DetDisplayLib.py
@@ -1,7 +1,9 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/DetDisplay/DetDisplayLib.py,v 1.1 2008/08/15 21:42:34 ecephas Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/DetDisplay/DetDisplayLib.py,v 1.2 2008/11/12 17:51:29 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['DetDisplay'])
     env.Tool('TkrUtilLib')
+    env.Tool('GlastSvcLib')
+    env.Tool('geomrepLib')
 def exists(env):
     return 1;
