5c6f19f19d797a19671daccab489f3b9c780f24a
jrb <>
2012-08-18 00:23:52 +0000

    support no-install headers on Windows

--- DetDisplayLib.py
+++ DetDisplayLib.py
@@ -1,9 +1,11 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/DetDisplay/DetDisplayLib.py,v 1.2 2008/11/12 17:51:29 ecephas Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/DetDisplay/DetDisplayLib.py,v 1.3 2009/11/06 23:31:01 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['DetDisplay'])
     env.Tool('TkrUtilLib')
     env.Tool('GlastSvcLib')
     env.Tool('geomrepLib')
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'GuiSvc') 
 def exists(env):
     return 1;
